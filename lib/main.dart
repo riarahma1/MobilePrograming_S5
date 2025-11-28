77481b515833cbe52b6b19cd6ff518f4031bcb78
riarahma1 <riarahmayatinurhumairoh@saintek.unipdu.ac.id>
2025-11-28 18:03:27 +0700

    Praktikum8

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,8 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'models/user.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:riaproject_mobileprograming/Praktikum5/bottomsheet.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: UserPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class UserPage extends StatelessWidget {
-  const UserPage({super.key});
-
-  Future<List<User>> fetchUsers() async {
-    try {
-      final response =
-      await http.get(Uri.parse('https://reqres.in/api/users?page=2'));
-
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        final List usersData = data['data'];
-        return usersData.map((json) => User.fromJson(json)).toList();
-      } else {
-        throw Exception('Status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      print("Error saat fetch data: $e");
-      rethrow;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Daftar Pengguna')),
-      body: FutureBuilder<List<User>>(
-        future: fetchUsers(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                'Terjadi kesalahan: ${snapshot.error}',
-                style: const TextStyle(color: Colors.red),
-              ),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('Tidak ada data pengguna.'));
-          } else {
-            final users = snapshot.data!;
-            return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(user.avatar),
-                  ),
-                  title: Text('${user.firstName} ${user.lastName}'),
-                  subtitle: Text(user.email),
-                );
-              },
-            );
-          }
-        },
-      ),
-    );
-  }
+void main() async {
+  await GetStorage.init();
+  runApp(MyApp());
 }
